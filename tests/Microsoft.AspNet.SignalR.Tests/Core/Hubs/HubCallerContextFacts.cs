bfbad53c00cc3345a922a1fd583b1ed5873516d0
Abhishek Nanda <abnanda@microsoft.com>
2014-04-28 19:23:59 -0700

    Made changes as per code review feedback
    #3015

---
+++ tests/Microsoft.AspNet.SignalR.Tests/Core/Hubs/HubCallerContextFacts.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.Tests.Core.Hubs
+{
+    public class HubCallerContextFacts
+    {
+        [Fact]
+        public void HubCallerContextCanBeMocked()
+        {
+            var hubCallerContext = new Mock<HubCallerContext>();
+            hubCallerContext.Setup<string>(h => h.ConnectionId).Returns("test");
+            Assert.Equal("test", hubCallerContext.Object.ConnectionId);
+        }
+    }
+}
